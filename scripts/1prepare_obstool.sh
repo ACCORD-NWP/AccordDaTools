c3dfec6dac208a8299ef6dc3dd226eb08ed59b4f
Eoin Whelan <eoin.whelan@met.ie>
2022-02-01 15:43:10 +0000

    Get conventional processing up and running

--- scripts/1prepare_obstool.sh
+++ scripts/1prepare_obstool.sh
@@ -1,84 +1,130 @@
+#!/bin/bash
+
+#set -xe
+set -e
+
 #1prepare_obstool.sh
 #Once we have ascii files with odbsql we start with thi script to prepare the data.
- 
 
-#!/bin/bash
-module load R
+PROGNAME=`basename $0`
 
-# Definitions
+# Define a usage function
+usage() {
 
-#Charge .profile
-#. ./profile_obstool.h
+bold=$(tput bold)
+normal=$(tput sgr0)
+unline=$(tput smul)
 
-#or you can have data here:
-#---------------------
-# Experiment Setting
-#---------------------
-#lEXP="AIBr_fr"
-lEXP="AIBr_fr2"
-cEXP="oper"
-#Be careful: also write exp name in Rscrits/preparing_gnss.R and monitoring_gnss.R
-#---------------------
-# Setting Date
-#---------------------
-sdate=2019021100
-edate=2019022821
-#sdate=2019031712
-#edate=2019032606
-cycle=3
-#Observation
-OBS=gnss_ztd
+cat << USAGE
 
-#........................
+${bold}NAME${normal}
+        ${PROGNAME} - estimate observation errors
 
-mandtg=/home/ms/es/mdy/bin/mandtg.pl
+${bold}USAGE${normal}
+        ${PROGNAME} -i <input-directory> -o <output-directory>
+                    -s <start-dtg> -e <end-dtg>
+                    [ -h ]
 
-echo ${lEXP} 
+${bold}DESCRIPTION${normal}
+        Prepare input data for ObsTool error covariance estimation
 
-#READ=".\/Files\/"
-DIRW=/AccordDaTools/src
+${bold}OPTIONS${normal}
+        -i ${unline}input-directory${normal}
+           input directory
+
+        -o ${unline}output-directory${normal}
+           output directory
+
+        -s ${unline}start-dtg${normal}
+
+        -e ${unline}end-dtg${normal}
+
+        -c ${unline}forecast-cycle${normal}
+
+        -n ${unline}expt-name${normal}
+           Experiment name
+
+        -h Help! Print usage information.
+
+USAGE
+} 
+
+SDTG=DUMMY
+EDTG=DUMMY
+FINT=3
+NEXP="DUMMY"
+
+while getopts i:o:s:e:c:n:h option
+do
+  case $option in
+    i)
+       OTIN=$OPTARG
+       ;;
+    o)
+       OTOUT=$OPTARG
+#EW       if [ -d $OTOUT ]; then
+#EW         echo "Directory ${OTOUT} already exists. Please choose another name"
+#EW         exit 1
+#EW       else
+#EW         mkdir -p ${OTOUT}
+#EW       fi
+       ;;
+    s)
+       SDTG=$OPTARG
+       ;;
+    e)
+       EDTG=$OPTARG
+       ;;
+    c)
+       FINT=$OPTARG
+       ;;
+    n)
+       NEXP=$OPTARG
+       ;;
+    h)
+       usage
+       exit 0
+       ;;
+    *)
+       echo
+       echo "Try '${PROGNAME} -h' for more information"
+       ;;
+  esac
+done
+
+#
+# Where am I?
+#
+this_script_loc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+appdir=$(dirname ${this_script_loc})
+bindir=${appdir}/bin
+libdir=${appdir}/lib
+exedir=${appdir}/libexec
+levdir=${appdir}/share/levdef
 
 #For gnss ztd, radar, radar_dow  and also for ascat: 
 exe=preparing_conv.R
-#For other satellite data: 
-#exe=preparing_sat.R
-
-echo $sdate
-echo $edate
-echo $cycle
-date=$sdate
 
 #==========================================================
 # 		PREPARE STATISTICS
 #==========================================================
-cd ${DIRW}
+CDTG=$SDTG
+while [[ $CDTG -le $EDTG ]]; do
+  echo
+  echo "Processing CCMA data for ${CDTG} ..."
+  if [ ! -s ${OTIN}/ccma_mon_conv_${CDTG} ]; then
+    echo "  ${PROGNAME}: Input ${OTIN}/ccma_mon_conv_${CDTG} does not exist."
+    echo "  ${PROGNAME}: Aborting ..."
+    exit 1
+  fi
+  sed -e "s/XXYYMMDDNTXX/${CDTG}/" ${exedir}/$exe > ${CDTG}_${exe}
 
-  while [[ $date -le $edate ]]
-  do
+  # Start visualization in R
+  echo "R CMD BATCH ${CDTG}_${exe}"
+  R CMD BATCH ${CDTG}_${exe} && rm -f ${CDTG}_${exe}
+  # increment by cycle h
+  DSTR="${CDTG:0:4}-${CDTG:4:2}-${CDTG:6:2} ${CDTG:8:2}:00:00"
+  CDTG=$(date -d "$DSTR $FINT hour" +%Y%m%d%H)
+done
 
-   echo "preparing $date"
-
-
-   for iEXP in ${lEXP}
-   do 
-    sed \
-		-e "s/exp=\"\"/exp=\"${iEXP}\"/"     \
-        -e "s/XXYYMMDDNTXX/${date}/"         \
-        $exe > run1.${iEXP}.x
-
-# Start visualization in R
-		R CMD BATCH run1.${iEXP}.x
-   done
-
-   # increment by cycle h
-   #date=`expr ${date} + ${cycle}`
-   date=`$mandtg ${date} + ${cycle}`
-  done
-
-  echo 'Result at /../$OBS/'${lEXP}
-  echo 'Output at: ~/../src/run1.${lEXP}.x.Rout'
-
-
-# Cleaning
-			rm -f run*.x  
-
+exit 0
--- src/preparing_conv.R
+++ src/preparing_conv.R
@@ -1,111 +1,95 @@
 
 #########################################
-# 	SETTINGS
+# SETTINGS
 #########################################
 #january 2022. Jana. Some cleaning from sat data.
 #This script can be used for gnssztd, radar, or any conv obs.And ascat. 
 #Be CAREFUL: Fill the path input in read0 and also the name of the file
 
-# Set satellite/sensor ID
-mylsat  = c(223)
-mylsens = c(3)
-
 # Spatial separation bin for OmG
-bin_int = 20 		# [km] : binning interval for pairs
-bin_max_dist = 200 	# [km] : maximal distance for the bin_int
+bin_int = 20 # [km] : binning interval for pairs
+bin_max_dist = 200 # [km] : maximal distance for the bin_int
 
 # Time separation distance 
-time_btw_omg = 60	# [+/- min] : time_btw_omg = time between OmG/OmA in pairs
+time_btw_omg = 60# [+/- min] : time_btw_omg = time between OmG/OmA in pairs
 dist=20
 
 #########################################
-#	START SCRIPT
+#START SCRIPT
 #########################################
 # Load R-packages
 library(sp)
 
 # INPUTS
-exp=""
 yymmddnt="XXYYMMDDNTXX"
 
 lDint = c(0, 1, seq(bin_int, bin_max_dist, bin_int))
 cDint = c(0, seq(bin_int, bin_max_dist, bin_int))
 
 # SET VARIABLES
-#read="/scratch/ms/es/mdy/HARM_DiagTool/gnss_obs"
-#save="/scratch/ms/es/mdy/HARM_DiagTool/gnss_obs"
-
-read0="/.../...PATHINPUT"
-read= paste(read0,"/",exp, sep="")
+read="input"
 save= read
 
 date = strptime(as.character(yymmddnt), format="%Y%m%d%H")
-#lab_input.x = c("satid", "press", "sensor", "lat", "lon", "an_depar", "tdiff", "fg_depar", "obsvalue")
-lab_input.x = c("satid","lat","lon","an_depar","fg_depar","obsvalue")
-
+#$~> odbsql -T -q 'SELECT rad2deg(lat),rad2deg(lon),an_depar,fg_depar,obsvalue FROM hdr,body,desc where obstype=1 AND an_depar IS NOT NULL'
+lab_input.x = c("lat","lon","an_depar","fg_depar","obsvalue")
 
 # READ DATA
-#file = paste(read,"/",exp,"/ccma_gnss_", yymmddnt, sep="")
-#file = paste(read,"/",exp,"/ccma_mon_", yymmddnt, sep="")
-file = paste(read,"/ccma_mon_", yymmddnt, sep="")
+file = paste(read,"/ccma_mon_conv_", yymmddnt, sep="")
 #------------------------------------
-# 	RUN PREPARATION
+# RUN PREPARATION
 #------------------------------------
 if (file.exists(file)){ 
 
-  #rdata = read.table(paste(file,sep=""), col.names=lab_input.x)
+  print(paste("File exists: ",file))
+  print("Read data")
+
   data = read.table(paste(file,sep=""), col.names=lab_input.x)
   newdata = data.frame()
-
-  # DATA SELECTION
-  #data = subset(rdata, abs(tdiff) <= time_btw_omg)
 
   #------------------------------------
   # START DISTANCE CALCULATION
   #------------------------------------
-  
+  # Distances between satellite pixels
+  # matrix(a,b) : diagonal(0 at points), non-diagonal(distance btw pixels)
+  # Distance is determined by spDists
 
-        # Distances between satellite pixels
-  	# matrix(a,b) : diagonal(0 at points), non-diagonal(distance btw pixels)
-  	# Distance is determined by spDists
+  latlon  <- SpatialPoints(coords = data[,c("lon","lat")], proj4string=CRS("+proj=longlat +datum=WGS84"))
+  matdist <- spDists(latlon, latlon, longlat=T)
+  print(paste("----> LatLon"))
 
-        latlon  <- SpatialPoints(coords = data[,c("lon","lat")], proj4string=CRS("+proj=longlat +datum=WGS84"))
-        matdist <- spDists(latlon, latlon, longlat=T)
+  tabdist = as.table(matdist)
+  nobs = length(data$obsvalue)
 
-        print(paste("----> LatLon"))
+  colnames(tabdist) = 1:nobs
+  rownames(tabdist) = 1:nobs
 
-        tabdist = as.table(matdist)
+  rdfnewdata = as.data.frame(tabdist)
+  names(rdfnewdata) = c("n1","n2","dist")
 
-        nobs = length(data$obsvalue)
+  dfnewdata = subset(rdfnewdata, dist<=bin_max_dist)
 
-  	colnames(tabdist) = 1:nobs
-  	rownames(tabdist) = 1:nobs
+  print(paste("----> Data Table"))
+  dfnewdata$OA1 = data[dfnewdata[,"n1"], "an_depar"]
+  dfnewdata$OA2 = data[dfnewdata[,"n2"], "an_depar"]
+  dfnewdata$FG1 = data[dfnewdata[,"n1"], "fg_depar"]
+  dfnewdata$FG2 = data[dfnewdata[,"n2"], "fg_depar"]
 
-  	rdfnewdata = as.data.frame(tabdist)
-  	names(rdfnewdata) = c("n1","n2","dist")
+  print(paste("----> newdata"))
+  newdata = rbind(newdata, dfnewdata)
 
-        #dfnewdata = subset(rdfnewdata, dist>0 & dist<=bin_max_dist)
-        dfnewdata = subset(rdfnewdata, dist<=bin_max_dist)
-
-        print(paste("----> Data Table"))
-
-        dfnewdata$OA1 = data[dfnewdata[,"n1"], "an_depar"]
-        dfnewdata$OA2 = data[dfnewdata[,"n2"], "an_depar"]
-        dfnewdata$FG1 = data[dfnewdata[,"n1"], "fg_depar"]
-        dfnewdata$FG2 = data[dfnewdata[,"n2"], "fg_depar"]
-                 
-	newdata = rbind(newdata, dfnewdata)
-
-  
+  print(paste("----> snewdata"))
   snewdata = subset(newdata, dist<=bin_max_dist)
+
+  print(paste("----> ldist"))
   ldist = cut(snewdata$dist, lDint, labels=cDint, include.lowest=TRUE)
 
+  print(paste("----> unique"))
   print(unique(ldist))
-
   attach(snewdata)
 
   ###########################################
-  # 		SAVE SCORES
+  # SAVE SCORES
   ###########################################
   # Save scores for each day 
   # --> less demaning for memory
@@ -127,17 +111,18 @@ if (file.exists(file)){
   # sum[FG1^2] ; sum[FG2^2] ; sum[OA1^2]
   #==============================================================
   # COVARIANCE, CORRELATIONS
-  StatTab        = aggregate(list(Asum1   = OA1)		, list(dist=ldist), FUN=sum    )
-  StatTab$FGsum1 = aggregate(list(FGSUM1  = FG1)		, list(dist=ldist), FUN=sum    )$FGSUM1
-  StatTab$FGsum2 = aggregate(list(FGSUM2  = FG2)		, list(dist=ldist), FUN=sum    )$FGSUM2
-  StatTab$AFGsqr = aggregate(list(AFGSQR  = OA1*FG2)		, list(dist=ldist), FUN=sum    )$AFGSQR
-  StatTab$FGsqr  = aggregate(list(FGSQR   = FG1*FG2)		, list(dist=ldist), FUN=sum    )$FGSQR
-  StatTab$num    = aggregate(list(NOBS    = FG2)		, list(dist=ldist), FUN=length )$NOBS
+  print(paste("----> StatTab"))
+  StatTab        = aggregate(list(Asum1   = OA1), list(dist=ldist), FUN=sum    )
+  StatTab$FGsum1 = aggregate(list(FGSUM1  = FG1), list(dist=ldist), FUN=sum    )$FGSUM1
+  StatTab$FGsum2 = aggregate(list(FGSUM2  = FG2), list(dist=ldist), FUN=sum    )$FGSUM2
+  StatTab$AFGsqr = aggregate(list(AFGSQR  = OA1*FG2), list(dist=ldist), FUN=sum    )$AFGSQR
+  StatTab$FGsqr  = aggregate(list(FGSQR   = FG1*FG2), list(dist=ldist), FUN=sum    )$FGSQR
+  StatTab$num    = aggregate(list(NOBS    = FG2), list(dist=ldist), FUN=length )$NOBS
 
   # STD
-  StatTab$FGsqr1 = aggregate(list(FGSQR1  = FG1*FG1)		, list(dist=ldist), FUN=sum )$FGSQR1
-  StatTab$FGsqr2 = aggregate(list(FGSQR2  = FG2*FG2)		, list(dist=ldist), FUN=sum )$FGSQR2
-  StatTab$Asqr1  = aggregate(list(ASQR1   = OA1*OA1)		, list(dist=ldist), FUN=sum )$ASQR1
+  StatTab$FGsqr1 = aggregate(list(FGSQR1  = FG1*FG1), list(dist=ldist), FUN=sum )$FGSQR1
+  StatTab$FGsqr2 = aggregate(list(FGSQR2  = FG2*FG2), list(dist=ldist), FUN=sum )$FGSQR2
+  StatTab$Asqr1  = aggregate(list(ASQR1   = OA1*OA1), list(dist=ldist), FUN=sum )$ASQR1
 
   detach(snewdata)
 
@@ -146,13 +131,10 @@ if (file.exists(file)){
   StatTab$dist   = as.numeric(as.character(StatTab[,"dist"]))
 
   # SAVE STATISTICS
-  #save(StatTab, file=paste(save,"/",exp,"/stat_sats_",yymmddnt,".Rdat",sep=""))
-  save(StatTab, file=paste(save,"/stat_sats_",yymmddnt,".Rdat",sep=""))
+  save(StatTab, file=paste(save,"/stat_conv_",yymmddnt,".Rdat",sep=""))
 
 }else{
-
   print(paste("File",file,"does not exist!!"))
-
 }
 
 rm(list = ls())
