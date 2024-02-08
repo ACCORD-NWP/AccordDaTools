345c374d46feb2fb79891037051de981ac99709e
Swapan Mallick <swapanmallicksmhi@gmail.com>
2024-02-08 11:13:51 +0000

    Festat script updated

--- scripts/festat_S1.py
+++ scripts/festat_S1.py
@@ -2,47 +2,38 @@
 import os
 from datetime import datetime, timedelta
 
-#---------INPUT NEEDED-----------
-EXP    = "SURF_CARRA2_CY46"              # Change the experiment name here
-dtgbeg =  datetime(2022, 1, 15, 00)       # Change Start date 
-dtgend =  datetime(2022, 2, 12, 18)       # Change End date
+#---------INPUT STAFF-----------
+EXP    = "SURF_CARRA2_CY46"                            # Change the experiment name here
+dtg    = datetime.strptime('2022011500', '%Y%m%d%H')   # Change Start date
+dtgend = datetime.strptime('2022021218', '%Y%m%d%H')   # Change End date
 #--------------------------------
-#
-cycl =  timedelta(hours=6)                # Assimilation cycle at 00 06 12 and 18 UTC
+cycl =  timedelta(hours=6)                             # Assimilation cycle at 00 06 12 and 18 UTC
 SCRATCH = os.environ['SCRATCH']
 HM_DATA = os.path.join(SCRATCH,'hm_home',EXP,'archive')
-TMPOUT  = os.path.join(SCRATCH,'tmp_festat') # Working directory 'tmp_festat' 
+TMPOUT  = os.path.join(SCRATCH,'tmp_festat')           # Working directory 'tmp_festat' 
 os.makedirs(TMPOUT, mode=0o755, exist_ok=True)
 os.chdir(TMPOUT)
-ens_mem=range(1,10)                       # Ensemble member (ens1,ens2.....,ens9)
-filename = "ICMSHHARM+0006"               # 6 hour forecast FA file
+ens_mem=range(1,10)                                    # Ensemble member (ens1,ens2.....,ens9)
+filename = "ICMSHHARM+0006"                            # 6 hour forecast FA file
 fnumber=1
 #
-while dtgbeg <= dtgend:
-    YY=dtgbeg.isoformat()[0:4]
-    MM=dtgbeg.isoformat()[5:7]
-    DD=dtgbeg.isoformat()[8:10]
-    HH=dtgbeg.isoformat()[11:13]
+while dtg <= dtgend:
+    #
+    YY = str(dtg.year).ljust(4, '0')
+    MM = str(dtg.month).zfill(2)
+    DD = str(dtg.day).zfill(2)
+    HH = str(dtg.hour).zfill(2)
+    # 
     for mm in ens_mem:
         mem='mbr00'.ljust(6,str(mm))
         target = os.path.join(HM_DATA, YY, MM, DD, HH, mem, filename)
-        print(target)
-        #
-        if   ( fnumber <= 9 ):
-            filelink = filename[:-1]+str(fnumber)
-        elif ( fnumber >= 10 and fnumber <= 99 ):
-            filelink = filename[:-2]+str(fnumber)
-        elif ( fnumber >= 100 and fnumber <= 999 ):
-            filelink = filename[:-3]+str(fnumber)
-        else:
-            break
-        #
-        if os.path.isfile(filelink):
-           os.remove(filelink)
-        os.symlink(target ,filelink)
+        filelink = filename[:-4] + str(fnumber).zfill(4)
+        if os.path.isfile(str(filelink)):
+              os.remove(str(filelink))
+        os.symlink(str(target),str(filelink))
+        print(str(filelink),str(target))
         fnumber += 1
-
-    dtgbeg += cycl
+    dtg += cycl
 #
 print(TMPOUT)
 quit()
