61f5b349a2e96e6e6d2b54284fbd1a7426116ef1
rmibdev <rmib.dev@gmail.com>
2026-03-15 18:08:21 +0000

    replace import odb by import read_odb

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,7 +3,7 @@
 
 from   modules.sigma_bo            import  Predef, Diag , Ratios
 from   modules.config_env          import  TuneEnv
-from   modules.odb                 import  Odb
+from   modules.read_odb            import  Odb
 from   modules.gsacov              import  GSA 
 
 __all__ = [ "Predef" , 
