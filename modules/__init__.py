48b7cabc9c094df507b72e2182477efffc2a276a
idir Dehmous <idirdehmous@gmail.com>
2026-03-21 21:58:24 +0100

    use the directory modules as a package path

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,18 +1,17 @@
 #-*- cvoding: utf-8 -*-
 
 # TuneBR modules 
-import handle_df
-import gsacov
-import config_env
-import sigma_bo
-import read_odb
+from  . import gsacov
+from  . import config_env
+from  . import sigma_bo
+from  . import read_odb
 
 
 # Obstool modules 
-import conv_stats
-import build_sql
-import obstype_info
-import io_base
-import setting
-import utils
+from  . import conv_stats
+from  . import build_sql
+from  . import obstype_info
+from  . import io_base
+from  . import setting
+from  . import utils
  
